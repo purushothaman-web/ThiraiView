8c2a6f20818e8d9e90a24b7fbe1fb69020c869cc
Purushothaman R <purushothravi7@gmail.com>
2025-09-10 21:36:34 +0530

    fix

--- Frontend/src/pages/AddMovie.jsx
+++ Frontend/src/pages/AddMovie.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
-import { isNonEmptyString, isYear } from "../components/ui/Validation";
+import { isNonEmptyString, isYear } from "../components/ui/validation";
 
 const AddMovie = () => {
   const { user, apiClient } = useContext(AuthContext);
